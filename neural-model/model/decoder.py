52791121c913106279f4d7857739c2a5d23bb49d
Pengcheng Yin <pcyin@cs.cmu.edu>
2019-10-01 21:56:15 -0400

    remove neural model

--- neural-model/model/decoder.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-
-
-class Decoder(nn.Module):
-    def __init__(self):
-        super(Decoder, self).__init__()
-
-    def forward(self, *input):
-        raise NotImplementedError
