b3e3379f4e6ad189c472b285d91fb8faffc94b6d
Jeremy Lacomis <jlacomis@cmu.edu>
2019-10-07 14:31:08 -0400

    Reformat embedding.py

--- prediction-plugin/model/embedding.py
+++ prediction-plugin/model/embedding.py
@@ -16,16 +16,23 @@ class SubTokenEmbedder(nn.Module):
         self.bpe_model.load(bpe_model_path)
         self.size = len(self.bpe_model)
         self.pad_id = self.bpe_model.pad_id()
-        self.embeddings = nn.Embedding(self.size, embedding_size, padding_idx=self.pad_id)
+        self.embeddings = nn.Embedding(self.size,
+                                       embedding_size,
+                                       padding_idx=self.pad_id)
 
     @property
     def device(self):
         return self.embeddings.weight.device
 
     @classmethod
-    def to_input_tensor(cls, sub_tokens_list: List[List[str]], bpe_model: spm.SentencePieceProcessor, pad_id=0) -> torch.Tensor:
+    def to_input_tensor(cls,
+                        sub_tokens_list: List[List[str]],
+                        bpe_model: spm.SentencePieceProcessor,
+                        pad_id=0) -> torch.Tensor:
         max_subword_num = max(len(x) for x in sub_tokens_list)
-        idx_tensor = torch.zeros(len(sub_tokens_list), max_subword_num, dtype=torch.long)
+        idx_tensor = torch.zeros(len(sub_tokens_list),
+                                 max_subword_num,
+                                 dtype=torch.long)
         idx_tensor.fill_(pad_id)
 
         for i, token_list in enumerate(sub_tokens_list):
@@ -35,7 +42,8 @@ class SubTokenEmbedder(nn.Module):
         return idx_tensor
 
     def get_embedding(self, sub_tokens_list: List[List[str]]) -> torch.Tensor:
-        idx_tensor = self.to_input_tensor(sub_tokens_list, self.bpe_model, self.pad_id)
+        idx_tensor = \
+            self.to_input_tensor(sub_tokens_list, self.bpe_model, self.pad_id)
         embedding = self.forward(idx_tensor)
 
         return embedding
@@ -43,9 +51,10 @@ class SubTokenEmbedder(nn.Module):
     def forward(self, sub_tokens_indices: torch.Tensor) -> torch.Tensor:
         # sub_tokens_indices: (batch_size, max_sub_token_num)
         sub_tokens_mask = torch.ne(sub_tokens_indices, self.pad_id).float()
-        embedding = self.embeddings(sub_tokens_indices) * sub_tokens_mask.unsqueeze(-1)
-        embedding = embedding.sum(dim=1) / sub_tokens_mask.sum(-1).unsqueeze(-1)
-
+        embedding = \
+            self.embeddings(sub_tokens_indices) * sub_tokens_mask.unsqueeze(-1)
+        embedding = \
+            embedding.sum(dim=1) / sub_tokens_mask.sum(-1).unsqueeze(-1)
         return embedding
 
 
@@ -55,7 +64,8 @@ class NodeTypeEmbedder(nn.Module):
 
         self.size = type_num
         self.pad_id = pad_id
-        self.embeddings = nn.Embedding(type_num, embedding_size, padding_idx=self.pad_id)
+        self.embeddings = \
+            nn.Embedding(type_num, embedding_size, padding_idx=self.pad_id)
 
     @property
     def device(self):
@@ -66,18 +76,24 @@ class NodeTypeEmbedder(nn.Module):
                         type2id: Dict[str, int],
                         pad_id=0) -> torch.Tensor:
         max_subword_num = max(len(x) for x in token_types_list)
-        idx_tensor = torch.zeros(len(token_types_list), max_subword_num, dtype=torch.long)
+        idx_tensor = torch.zeros(len(token_types_list),
+                                 max_subword_num,
+                                 dtype=torch.long)
         idx_tensor.fill_(pad_id)
 
         for i, token_list in enumerate(token_types_list):
-            idx_tensor[i, :len(token_list)] = torch.tensor([type2id[t] for t in token_list])
-
+            idx_tensor[i, :len(token_list)] = \
+                torch.tensor([type2id[t] for t in token_list])
         return idx_tensor
 
     def forward(self, type_tokens_indices: torch.Tensor) -> torch.Tensor:
         # type_tokens_indices: (batch_size, max_sub_token_num)
         sub_tokens_mask = torch.ne(type_tokens_indices, self.pad_id).float()
-        embedding = self.embeddings(type_tokens_indices) * sub_tokens_mask.unsqueeze(-1)
-        embedding = embedding.sum(dim=1) / (sub_tokens_mask.sum(-1) + SMALL_NUMBER).unsqueeze(-1)
+        embedding = \
+            self.embeddings(type_tokens_indices) \
+            * sub_tokens_mask.unsqueeze(-1)
+        embedding = \
+            embedding.sum(dim=1) \
+            / (sub_tokens_mask.sum(-1) + SMALL_NUMBER).unsqueeze(-1)
 
         return embedding
