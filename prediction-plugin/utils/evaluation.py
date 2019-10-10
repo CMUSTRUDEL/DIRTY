c8a4e0cb23917f058d2f23eacbb4d74ad37e70cb
Jeremy Lacomis <jlacomis@cmu.edu>
2019-10-10 13:37:59 -0400

    Reformat evaluation.py

--- prediction-plugin/utils/evaluation.py
+++ prediction-plugin/utils/evaluation.py
@@ -30,7 +30,7 @@ class Evaluator(object):
                 agg_results.setdefault(key, []).append(val)
 
         avg_results = dict()
-        avg_results['corpus_cer'] = 0# sum(agg_results['edit_distance']) / sum(agg_results['ref_len'])
+        avg_results['corpus_cer'] = 0
 
         for key, val in agg_results.items():
             avg_results[key] = np.average(val)
@@ -38,18 +38,24 @@ class Evaluator(object):
         return avg_results
 
     @staticmethod
-    def evaluate_ppl(model: RenamingModel, dataset: Dataset, config: Dict, predicate: Any = None):
+    def evaluate_ppl(model: RenamingModel,
+                     dataset: Dataset,
+                     config: Dict,
+                     predicate: Any = None):
         if predicate is None:
-            predicate = lambda e: True
+            def predicate(_):
+                return True
 
         eval_batch_size = config['train']['batch_size']
+        num_readers = config['train']['num_readers']
+        num_batchers = config['train']['num_batchers']
         data_iter = dataset.batch_iterator(batch_size=eval_batch_size,
                                            train=False, progress=True,
                                            return_examples=False,
                                            return_prediction_target=True,
                                            config=model.config,
-                                           num_readers=config['train']['num_readers'],
-                                           num_batchers=config['train']['num_batchers'])
+                                           num_readers=num_readers,
+                                           num_batchers=num_batchers)
 
         was_training = model.training
         model.eval()
@@ -57,10 +63,11 @@ class Evaluator(object):
         cum_num_examples = 0
         with torch.no_grad():
             for batch in data_iter:
-                nn_util.to(batch.tensor_dict, model.device)
-                result = model(batch.tensor_dict, batch.tensor_dict['prediction_target'])
+                td = batch.tensor_dict
+                nn_util.to(td, model.device)
+                result = model(td, td['prediction_target'])
                 log_probs = result['batch_log_prob'].cpu().tolist()
-                for e_id, test_meta in enumerate(batch.tensor_dict['test_meta']):
+                for e_id, test_meta in enumerate(td['test_meta']):
                     if predicate(test_meta):
                         log_prob = log_probs[e_id]
                         cum_log_probs += log_prob
@@ -83,13 +90,15 @@ class Evaluator(object):
                 eval_batch_size = config['train']['eval_batch_size']
             else:
                 eval_batch_size = config['train']['batch_size']
+        num_readers = config['train']['num_readers']
+        num_batchers = config['train']['num_batchers']
         data_iter = dataset.batch_iterator(batch_size=eval_batch_size,
                                            train=False,
                                            progress=True,
                                            return_examples=True,
                                            config=model.config,
-                                           num_readers=config['train']['num_readers'],
-                                           num_batchers=config['train']['num_batchers'])
+                                           num_readers=num_readers,
+                                           num_batchers=num_batchers)
         model.eval()
         all_examples = dict()
 
@@ -100,11 +109,12 @@ class Evaluator(object):
                 for example, rename_result in zip(examples, rename_results):
                     example_pred_accs = []
                     top_rename_result = rename_result[0]
-                    for old_name, gold_new_name in example.variable_name_map.items():
+                    for old_name, gold_new_name \
+                            in example.variable_name_map.items():
                         pred = top_rename_result[old_name]
                         pred_new_name = pred['new_name']
-                        var_metric = \
-                            Evaluator.get_soft_metrics(pred_new_name, gold_new_name)
+                        var_metric = Evaluator.get_soft_metrics(pred_new_name,
+                                                                gold_new_name)
                         example_pred_accs.append(var_metric)
                     file_name = example.binary_file['file_name']
                     line_num = example.binary_file['line_num']
@@ -114,15 +124,24 @@ class Evaluator(object):
         return all_examples
 
     @staticmethod
-    def decode_and_evaluate(model: RenamingModel, dataset: Dataset, config: Dict, return_results=False, eval_batch_size=None):
+    def decode_and_evaluate(model: RenamingModel,
+                            dataset: Dataset,
+                            config: Dict,
+                            return_results=False,
+                            eval_batch_size=None):
         if eval_batch_size is None:
-            eval_batch_size = config['train']['eval_batch_size'] if 'eval_batch_size' in config['train'] else config['train']['batch_size']
+            if 'eval_batch_size' in config['train']:
+                eval_batch_size = config['train']['eval_batch_size']
+            else:
+                eval_batch_size = config['train']['batch_size']
+        num_readers = config['train']['num_readers']
+        num_batchers = config['train']['num_batchers']
         data_iter = dataset.batch_iterator(batch_size=eval_batch_size,
                                            train=False, progress=True,
                                            return_examples=True,
                                            config=model.config,
-                                           num_readers=config['train']['num_readers'],
-                                           num_batchers=config['train']['num_batchers'])
+                                           num_readers=num_readers,
+                                           num_batchers=num_batchers)
 
         was_training = model.training
         model.eval()
@@ -130,10 +149,10 @@ class Evaluator(object):
         variable_acc_list = []
         need_rename_cases = []
 
-        func_name_in_train_acc_list = []
-        func_name_not_in_train_acc_list = []
-        func_body_in_train_acc_list = []
-        func_body_not_in_train_acc_list = []
+        func_name_in_train_acc = []
+        func_name_not_in_train_acc = []
+        func_body_in_train_acc = []
+        func_body_not_in_train_acc = []
 
         all_examples = dict()
 
@@ -145,32 +164,38 @@ class Evaluator(object):
                     example_pred_accs = []
 
                     top_rename_result = rename_result[0]
-                    for old_name, gold_new_name in example.variable_name_map.items():
+                    for old_name, gold_new_name \
+                            in example.variable_name_map.items():
                         pred = top_rename_result[old_name]
                         pred_new_name = pred['new_name']
-                        var_metric = Evaluator.get_soft_metrics(pred_new_name, gold_new_name)
+                        var_metric = Evaluator.get_soft_metrics(pred_new_name,
+                                                                gold_new_name)
                         # is_correct = pred_new_name == gold_new_name
                         example_pred_accs.append(var_metric)
 
-                        if gold_new_name != old_name:  # and gold_new_name in model.vocab.target:
+                        if gold_new_name != old_name:
                             need_rename_cases.append(var_metric)
 
                             if example.test_meta['function_name_in_train']:
-                                func_name_in_train_acc_list.append(var_metric)
+                                func_name_in_train_acc.append(var_metric)
                             else:
-                                func_name_not_in_train_acc_list.append(var_metric)
+                                func_name_not_in_train_acc.append(var_metric)
 
                             if example.test_meta['function_body_in_train']:
-                                func_body_in_train_acc_list.append(var_metric)
+                                func_body_in_train_acc.append(var_metric)
                             else:
-                                func_body_not_in_train_acc_list.append(var_metric)
+                                func_body_not_in_train_acc.append(var_metric)
 
                     variable_acc_list.extend(example_pred_accs)
                     example_acc_list.append(example_pred_accs)
 
                     if return_results:
-                        all_examples[example.binary_file['file_name'] + '_' + str(example.binary_file['line_num'])] = (rename_result, Evaluator.average(example_pred_accs))
-                        # all_examples.append((example, rename_result, example_pred_accs))
+                        example = \
+                            f"{example.binary_file['file_name']}_" \
+                            f"{example.binary_file['line_num']}"
+                        all_examples[example] = \
+                            (rename_result,
+                             Evaluator.average(example_pred_accs))
 
         valid_example_num = len(example_acc_list)
         num_variables = len(variable_acc_list)
@@ -179,12 +204,17 @@ class Evaluator(object):
         if was_training:
             model.train()
 
+        need_rename_acc = Evaluator.average(need_rename_cases)
+        name_in_train_acc = Evaluator.average(func_name_in_train_acc)
+        name_not_in_train_acc = Evaluator.average(func_name_not_in_train_acc)
+        body_in_train_acc = Evaluator.average(func_body_in_train_acc)
+        body_not_in_train_acc = Evaluator.average(func_body_not_in_train_acc)
         eval_results = dict(corpus_acc=corpus_acc,
-                            corpus_need_rename_acc=Evaluator.average(need_rename_cases),
-                            func_name_in_train_acc=Evaluator.average(func_name_in_train_acc_list),
-                            func_name_not_in_train_acc=Evaluator.average(func_name_not_in_train_acc_list),
-                            func_body_in_train_acc=Evaluator.average(func_body_in_train_acc_list),
-                            func_body_not_in_train_acc=Evaluator.average(func_body_not_in_train_acc_list),
+                            corpus_need_rename_acc=need_rename_acc,
+                            func_name_in_train_acc=name_in_train_acc,
+                            func_name_not_in_train_acc=name_not_in_train_acc,
+                            func_body_in_train_acc=body_in_train_acc,
+                            func_body_not_in_train_acc=body_not_in_train_acc,
                             num_variables=num_variables,
                             num_valid_examples=valid_example_num)
 
