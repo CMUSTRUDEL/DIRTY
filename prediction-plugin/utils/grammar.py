234fd5e9342b6fe7f79de40f32be56b342da9357
Jeremy Lacomis <jlacomis@cmu.edu>
2019-10-10 14:55:15 -0400

    Reformat grammar.py

--- prediction-plugin/utils/grammar.py
+++ prediction-plugin/utils/grammar.py
@@ -3,8 +3,10 @@ class Grammar(object):
         self.syntax_types = list(sorted(syntax_types))
         self.variable_types = ['<pad>', '<unk>'] + list(sorted(variable_types))
 
-        self.syntax_type_to_id = {type: id for id, type in enumerate(self.syntax_types)}
-        self._variable_type_to_id = {type: id for id, type in enumerate(self.variable_types)}
+        self.syntax_type_to_id = \
+            {type: id for id, type in enumerate(self.syntax_types)}
+        self._variable_type_to_id = \
+            {type: id for id, type in enumerate(self.variable_types)}
 
     def variable_type_to_id(self, type_token):
         if type_token in self._variable_type_to_id:
