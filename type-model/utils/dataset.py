fdd7555d64ab1ee5f4fef3a598921d87ccd56784
Qibin Chen <chenqibin422@gmail.com>
2020-12-04 05:27:18 -0500

    Add naive most common type predictor

--- type-model/utils/dataset.py
+++ type-model/utils/dataset.py
@@ -352,10 +352,47 @@ class Dataset(wds.Dataset):
 
 
 if __name__ == "__main__":
-    config = json.loads(_jsonnet.evaluate_file('config.xfmr_sub.jsonnet'))
+    config = json.loads(_jsonnet.evaluate_file('config.xfmr.jsonnet'))
     dataset = Dataset("data1/dev-*.tar", config["data"])
     dataloader = torch.utils.data.DataLoader(
-        dataset, num_workers=0, batch_size=64, collate_fn=Dataset.collate_fn
+        dataset, num_workers=8, batch_size=64, collate_fn=Dataset.collate_fn
     )
-    for x in tqdm(dataloader):
-        pass
+    with open(config["data"]["typelib_file"]) as type_f:
+        typelib = TypeLibCodec.decode(type_f.read())
+    most_common_for_size = {}
+    types_model = dataset.vocab.types
+    for size in typelib:
+        freq, tp = typelib[size][0]
+        most_common_for_size[size] = types_model[str(tp)]
+    cnt = 0
+    preds_list = []
+    targets_list = []
+    for batch in tqdm(dataloader):
+        input_dict, target_dict = batch
+        targets = target_dict["target_type_id"][target_dict["target_mask"]]
+        preds = []
+        for mems, target in zip(target_dict["target_type_src_mems"], targets.tolist()):
+            size = mems[mems != 0].tolist()[-1] - 3
+            if size not in most_common_for_size:
+                preds.append(types_model.unk_id)
+                continue
+            preds.append(most_common_for_size[size])
+        preds_list.append(torch.tensor(preds))
+        targets_list.append(targets)
+    preds = torch.cat(preds_list)
+    targets = torch.cat(targets_list)
+    print(preds.shape, targets.shape)
+    from pytorch_lightning.metrics.functional.classification import accuracy, f1_score
+    import wandb
+    wandb.init(name="most_common", project="dire")
+    wandb.log({"test_acc": accuracy(preds, targets)})
+    wandb.log({"test_f1_macro": f1_score(preds, targets, class_reduction='macro')})
+    struct_set = set()
+    for idx, type_str in types_model.id2word.items():
+        if type_str.startswith("struct"):
+            struct_set.add(idx)
+    struc_mask = torch.zeros(len(targets), dtype=torch.bool)
+    for idx, target in enumerate(targets):
+        if target.item() in struct_set:
+            struc_mask[idx] = 1
+    wandb.log({"test_struc_acc": accuracy(preds[struc_mask], targets[struc_mask])})
